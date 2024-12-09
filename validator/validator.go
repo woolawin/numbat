881336c04566f0ec104ad36de4cc360160ac1675
Blair Harris <woolawin@gmail.com>
2024-12-09 22:49:21 +0100

    check let and proc types

--- validator/validator.go
+++ validator/validator.go
@@ -1,7 +1,88 @@
 package validator
 
-import "numbat/read"
+import (
+	"numbat/read"
+)
 
-func Check(source *read.Source) bool {
+type UnknownType struct {
+	TypeName string
+}
+
+type ValidationError interface {
+}
+
+type Validation struct {
+	errors []ValidationError
+}
+
+func Check(source *read.Source) Validation {
+	validation := Validation{}
+
+	if source.Program != nil {
+		checkProc(&validation, *source.Program)
+	}
+
+	for _, proc := range source.Procs {
+		checkProc(&validation, proc)
+	}
+
+	return validation
+}
+
+func checkProc(validation *Validation, proc read.Proc) {
+	if proc.ReturnType != nil {
+		checkType(validation, *proc.ReturnType)
+	}
+	for _, stmt := range proc.Statements {
+		checkStatement(validation, stmt)
+	}
+}
+
+func checkStatement(validation *Validation, statement read.Statement) {
+	if statement.Let != nil {
+		checkLet(validation, *statement.Let)
+	}
+}
+
+func checkLet(validation *Validation, let read.Let) {
+	if let.VarType != nil {
+		checkType(validation, *let.VarType)
+	}
+}
+
+func checkType(validation *Validation, typ read.Type) {
+	if isUnknownType(typ.Out.Name) {
+		validation.errors = append(validation.errors, UnknownType{TypeName: typ.Out.Name})
+	}
+	for _, in := range typ.In {
+		checkType(validation, *in.Typ)
+	}
+}
+
+func isUnknownType(name string) bool {
+	if name == "" {
+		return false
+	}
+	return !isBuiltInType(name)
+}
+
+func isBuiltInType(typeName string) bool {
+	var types = [...]string{
+		"Int16",
+		"Int32",
+		"Int64",
+		"Float32",
+		"Float64",
+		"Bool",
+		"Str",
+		"Byte",
+	}
+
+	for _, t := range types {
+		if typeName == t {
+			return true
+		}
+	}
+
 	return false
 }
--- validator/validator_test.go
+++ validator/validator_test.go
@@ -1,12 +1,96 @@
 package validator
 
 import (
+	"github.com/google/go-cmp/cmp"
 	"numbat/read"
 	"testing"
 )
 
 func TestTypes(t *testing.T) {
 	src := &read.Source{
-		Program: nil,
+		Program: &read.Proc{
+			Statements: []read.Statement{
+				{
+					Let: &read.Let{
+						VarName: "a",
+						VarType: &read.Type{Out: read.TypeOut{Name: "Int32"}},
+					},
+				},
+				{
+					Let: &read.Let{
+						VarName: "b",
+						VarType: &read.Type{Out: read.TypeOut{Name: "Str"}},
+					},
+				},
+				{
+					Let: &read.Let{
+						VarName: "c",
+						VarType: &read.Type{Out: read.TypeOut{Name: "FooBar"}},
+					},
+				},
+				{
+					Let: &read.Let{
+						VarName: "d",
+						VarType: &read.Type{Out: read.TypeOut{Name: ""}},
+					},
+				},
+			},
+		},
+		Procs: []read.Proc{
+			{
+				Name:       "get_num",
+				Statements: []read.Statement{},
+				ReturnType: &read.Type{
+					Out: read.TypeOut{Name: "Int32"},
+				},
+			},
+			{
+				Name:       "foo",
+				Statements: []read.Statement{},
+				ReturnType: &read.Type{
+					Out: read.TypeOut{Name: "Foo"},
+				},
+			},
+			{
+				Name:       "multiply",
+				Statements: []read.Statement{},
+				ReturnType: &read.Type{
+					In: []read.Param{
+						{
+							Typ: &read.Type{Out: read.TypeOut{Name: "Int32"}},
+						},
+						{
+							Typ: &read.Type{Out: read.TypeOut{Name: "IntX"}},
+						},
+					},
+					Out: read.TypeOut{Name: "Int64"},
+				},
+			},
+		},
+	}
+
+	actual := Check(src)
+
+	expected := Validation{
+		errors: []ValidationError{
+			UnknownType{
+				TypeName: "FooBar",
+			},
+			UnknownType{
+				TypeName: "Foo",
+			},
+			UnknownType{
+				TypeName: "IntX",
+			},
+		},
+	}
+
+	assert(t, actual, expected)
+}
+
+func assert(t *testing.T, actual Validation, expected Validation) {
+	diff := cmp.Diff(actual.errors, expected.errors)
+	if diff != "" {
+		t.Fatalf(diff)
 	}
 }
